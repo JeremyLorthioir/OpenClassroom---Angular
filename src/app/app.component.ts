2b7a4422a4f9e6dd5a43478dcb6cab2caecb8c5d
Lorthioir <jeremy.lorthioir24@gmail.com>
2021-06-12 11:57:08 +0200

    [Improve] More preparation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  posts = [
+  // posts = [
+  //   {
+  //     title: 'Mon premier post',
+  //     content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
+  //     loveIts: 1,
+  //     created_at: new Date()
+  //   },
+  //   {
+  //     title: 'Mon deuxième post',
+  //     content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
+  //     loveIts: 0,
+  //     created_at: new Date()
+  //   },
+  //   {
+  //     title: 'Encore un post',
+  //     content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
+  //     loveIts: -1,
+  //     created_at: new Date()
+  //   }
+  // ];
+
+  isAuth = false;
+
+  lastUpdate = new Promise((resolve) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
     {
-      title: 'Mon premier post',
-      content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
-      loveIts: 1,
-      created_at: new Date()
+      name: 'Machine à laver',
+      status: 'éteint'
     },
     {
-      title: 'Mon deuxième post',
-      content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
-      loveIts: 0,
-      created_at: new Date()
+      name: 'Frigo',
+      status: 'allumé'
     },
     {
-      title: 'Encore un post',
-      content: 'Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.',
-      loveIts: -1,
-      created_at: new Date()
+      name: 'Ordinateur',
+      status: 'éteint'
     }
   ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+}
 }
